9c849cb65306b98021d733ca8b2b0c1e4fc75df9
Dylan Rylee A. Dizon <109629195+dylanrylee@users.noreply.github.com>
2025-05-22 16:01:57 -0600

    update: new changes (final)

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ https://dashboard.emailjs.com/admin
 
 ### Firebase
 https://console.firebase.google.com/u/1/project/portfolio-cd702/firestore/databases/-default-/data/~2Fprojects~2FzdYXN8JY60Mc6U0iITuB
+
+### Tailwind CSS + React Router
+https://tailwindcss.com/docs/installation/framework-guides/react-router
--- portfolio/app/main.tsx
+++ portfolio/app/main.tsx
@@ -8,7 +8,7 @@ import "./app.css";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
